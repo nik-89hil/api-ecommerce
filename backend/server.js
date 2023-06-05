67f67d7d1c0d830e2e8257ad5afd2254f79dfbe2
Nitin Kumar <nikhilkumar19072002@gmail.com>
2023-06-05 16:13:49 +0530

    changement in fourth  cors

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,21 @@ app.use(express.urlencoded({extended:true}))
 
 // app.use(express.static(path.join(__dirname,"../frontendwork/build")))
 // console.log(path.join(__dirname,"../frontendwork/build"))
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+    );
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
 
+    );
+
+    next();
+
+})
 
 
 app.use("/api",bookRoutes);
